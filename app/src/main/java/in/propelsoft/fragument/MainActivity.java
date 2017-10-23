f5a3a6c7e9134ea1eb0c88dc910e7d69c4b3ae58
Suvindran <suvindran10@gmail.com>
2017-10-23 17:31:43 +0530

    First Commit

---
+++ app/src/main/java/in/propelsoft/fragument/MainActivity.java
@@ -0,0 +1,65 @@
+package in.example.fragument;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("toolbar");
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addfragment(new FirstFragment(),getTitle().toString());
+        adapter.addfragment(new SecondFragment(),getTitle().toString());
+        adapter.addfragment(new ThirdFragment(),getTitle().toString());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public class Adapter extends FragmentPagerAdapter {
+
+        List<Fragment> list = new ArrayList<>();
+        List<String> title = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addfragment(Fragment fragment, String tile) {
+            list.add(fragment);
+            title.add(tile);}
+
+            @Override
+            public Fragment getItem ( int position){
+                return list.get(position);
+            }
+
+            @Override
+            public int getCount () {
+                return list.size();
+            }
+        }
+    }
+
